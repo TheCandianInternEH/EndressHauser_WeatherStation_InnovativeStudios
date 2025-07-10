d85e0254ac2ccde5221269351c6a05dba63e0f77
TheCandianInternEH <rushil.saha@endress.com>
2025-07-10 09:38:56 -0400

    Update main.blocks, weather_start.ts

--- weather_start.ts
+++ weather_start.ts
@@ -17,13 +17,13 @@ namespace custom {
     }
 }
 
-//% weight=90 color=#FFD700 icon="☀"
-//% block="Sun On"
+
 namespace custom {
     /**
      * Starts Sun Lamp
      */
     //% block = "SUN ON"
+    //% weight=90 color=#FFD700 icon="☀"
     export function SunOn(): void {
         basic.forever(function () {
             serial.writeLine("Blink")
